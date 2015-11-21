3016aeef9ecaa47c144419535926f96f8925b395
Nicholas Reed <nicholas.a.reed@gmail.com>
2015-11-21 14:22:31 -0800

    mongoose

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGOLAB_URI);
+
+var Art = mongoose.model('Art', { 
+  name: String,
+  url: String
+});
+
+
 // Cross-Origin-Resource-Sharing (CORS)
 app.use(function (req, res, next) {
 
@@ -28,23 +38,32 @@ app.get('/', function (req, res) {
 
 // API route(s)
 app.get('/list', function (req, res) {
-  res.json([{
-  	id: 1,
-  	name: 'test1',
-    url: 'http://www.artsfblog.com/uploads/4/9/9/3/49933823/s738677920177686506_p120_i1_w640.jpeg'
-  },{
-  	id: 2,
-  	name: 'test2',
-    url: 'http://www.artsfblog.com/uploads/4/9/9/3/49933823/s738677920177686506_p121_i1_w561.jpeg'
-  },{
-    id: 3,
-    name: 'test3',
-    url: 'http://www.artsfblog.com/uploads/4/9/9/3/49933823/s738677920177686506_p118_i1_w640.jpeg'
-  },{
-    id: 4,
-    name: 'test4',
-    url: 'http://www.artsfblog.com/uploads/4/9/9/3/49933823/s738677920177686506_p122_i1_w640.jpeg'
-  }]);
+  Art.find({},function(err, results){
+    if(err){
+      console.error(err);
+      return res.send(err);
+    }
+
+    res.json(results);
+
+  });
+  // res.json([{
+  // 	id: 1,
+  // 	name: 'test1',
+  //   url: 'http://www.artsfblog.com/uploads/4/9/9/3/49933823/s738677920177686506_p120_i1_w640.jpeg'
+  // },{
+  // 	id: 2,
+  // 	name: 'test2',
+  //   url: 'http://www.artsfblog.com/uploads/4/9/9/3/49933823/s738677920177686506_p121_i1_w561.jpeg'
+  // },{
+  //   id: 3,
+  //   name: 'test3',
+  //   url: 'http://www.artsfblog.com/uploads/4/9/9/3/49933823/s738677920177686506_p118_i1_w640.jpeg'
+  // },{
+  //   id: 4,
+  //   name: 'test4',
+  //   url: 'http://www.artsfblog.com/uploads/4/9/9/3/49933823/s738677920177686506_p122_i1_w640.jpeg'
+  // }]);
 });
   
 // Start the server
